4ee356c14c587f3d7c0eadd26a7824fb405885fe
Milena Ivanova <milena_ii@abv.bg>
2017-10-13 05:32:36 -0900

    Add Json files, add initial file structure

---
+++ Tails/Tails/Models/Abstractions/IStudent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tails.Models.Abstractions
+{
+    public interface IStudent
+    {
+        int Id { get; set; }
+
+        int FirstName { get; set; }
+
+        int LastName { get; set; }
+
+    }
+}
