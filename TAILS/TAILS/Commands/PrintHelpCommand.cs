0af9535777c770585adb0a095ba7fa777bb53878
Milena Ivanova <milena_ii@abv.bg>
2017-10-18 17:59:47 +0300

    Print Help Command

--- TAILS/TAILS/Commands/PrintHelpCommand.cs
+++ TAILS/TAILS/Commands/PrintHelpCommand.cs
@@ -13,6 +13,12 @@ namespace TAILS.Commands
         public string Execute(IList<string> parameters)
         {
             StringBuilder sb = new StringBuilder();
+            sb.AppendLine("CreateStudent -> Input parameters: FirstName LastName Username CourseIds");
+            sb.AppendLine("DeleteStudent -> Input parameters: Id");
+            sb.AppendLine("GenerateSeatsMap -> Input parameters: examId hallId");
+            sb.AppendLine("UpdateExamDateTime -> Input parameters: examId newDateTime ");
+
+
             //print info for all 5 commands (what parameters does each one take and what does it do) 
             return sb.ToString();
         }
