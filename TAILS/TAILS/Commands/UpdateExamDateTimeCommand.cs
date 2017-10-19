239f9eda5296835fe5ec91d54a06aa68dda998ff
MartinIvanov90 <martin.g.ivanov@gmail.com>
2017-10-19 11:46:13 +0300

    UpdateExamDateTime rdy & validated

--- TAILS/TAILS/Commands/UpdateExamDateTimeCommand.cs
+++ TAILS/TAILS/Commands/UpdateExamDateTimeCommand.cs
@@ -2,6 +2,9 @@
 using Bytes2you.Validation;
 using TAILS.Commands.Contracts;
 using System.Collections.Generic;
+using System;
+using System.Linq;
+using TAILS.Models;
 
 namespace TAILS.Commands
 {
@@ -18,7 +21,32 @@ namespace TAILS.Commands
 
         public string Execute(IList<string> parameters)
         {
-            throw new System.NotImplementedException();
+            int examId = 0;
+
+            if (!int.TryParse(parameters[0], out examId))
+            {
+                throw new ArgumentException("examid is invalid");
+            }
+
+            Exam examToFind = context.Exams.Find(examId);
+
+            if(examToFind == null)
+            {
+                throw new ArgumentException("there is no such exam in the DB");
+            }
+
+            DateTime newDateTime;
+            var joinedDate = string.Join(" ", parameters.Skip(1));
+
+            if (!DateTime.TryParse(joinedDate, out newDateTime))
+            {
+                throw new ArgumentException("DateTime format is invalid");
+            }
+
+            examToFind.DateTime = newDateTime;
+            context.SaveChanges();
+
+            return $"Updated exam with id: {examId} to: {newDateTime.ToString()}.";
         }
     }
 }
