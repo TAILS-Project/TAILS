24a7ee1ba1d973b7368389cb692747bf535be951
Georgi Georgiev <georgiwess@gmail.com>
2017-10-20 09:15:48 +0300

    Bring back deleted Engine tests

--- TAILS/TAILS.UnitTests/Properties/AssemblyInfo.cs
+++ TAILS/TAILS.UnitTests/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("TAILS.UnitTests")]
+[assembly: AssemblyTitle("Tails.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TAILS.UnitTests")]
+[assembly: AssemblyProduct("Tails.UnitTests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("b634a272-263c-414b-8ed6-4c7304da9da2")]
+[assembly: Guid("1570fd15-c9dd-4365-a7ec-4960923dcb9f")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
---
+++ TAILS/Tails.UnitTests/Core/EngineTests/Constructor_Should.cs
@@ -0,0 +1,82 @@
+﻿using Moq;
+using System;
+using TAILS.Core;
+using TAILS.Data;
+using TAILS.Core.Providers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TAILS.UnitTests.Core.EngineTests
+{
+    [TestClass]
+    public class Constructor_Should
+    {
+        [TestMethod]
+        public void ReturnObject_WhenProperArgumentsAreGiven()
+        {
+            //Arrange
+            var readerMock = new Mock<IReader>();
+            var writerMock = new Mock<IWriter>();
+            var parserMock = new Mock<ICommandParser>();
+            var contextMock = new Mock<ITAILSEntities>();
+
+            //Act
+            var engineInstance =
+                new Engine(readerMock.Object, writerMock.Object, parserMock.Object, contextMock.Object);
+
+            //Assert
+            Assert.IsNotNull(engineInstance);
+        }
+
+        [TestMethod]
+        public void ThrowError_WhenNullReaderIsGiven()
+        {
+            //Arrange
+            var writerMock = new Mock<IWriter>();
+            var parserMock = new Mock<ICommandParser>();
+            var contextMock = new Mock<ITAILSEntities>();
+
+            //Act && Assert
+            Assert.ThrowsException<ArgumentNullException>(
+                () => new Engine(null, writerMock.Object, parserMock.Object, contextMock.Object));
+        }
+
+        [TestMethod]
+        public void ThrowError_WhenNullWriterIsGiven()
+        {
+            //Arrange
+            var readerMock = new Mock<IReader>();
+            var parserMock = new Mock<ICommandParser>();
+            var contextMock = new Mock<ITAILSEntities>();
+
+            //Act && Assert
+            Assert.ThrowsException<ArgumentNullException>(
+                () => new Engine(readerMock.Object, null, parserMock.Object, contextMock.Object));
+        }
+
+        [TestMethod]
+        public void ThrowError_WhenNullCommandParserIsGiven()
+        {
+            //Arrange
+            var readerMock = new Mock<IReader>();
+            var writerMock = new Mock<IWriter>();
+            var contextMock = new Mock<ITAILSEntities>();
+
+            //Act && Assert
+            Assert.ThrowsException<ArgumentNullException>(
+                () => new Engine(readerMock.Object, writerMock.Object, null, contextMock.Object));
+        }
+
+        [TestMethod]
+        public void ThrowError_WhenNullContextIsGiven()
+        {
+            //Arrange
+            var readerMock = new Mock<IReader>();
+            var writerMock = new Mock<IWriter>();
+            var parserMock = new Mock<ICommandParser>();
+
+            //Act && Assert
+            Assert.ThrowsException<ArgumentNullException>(
+                () => new Engine(readerMock.Object, writerMock.Object, parserMock.Object, null));
+        }
+    }
+}
